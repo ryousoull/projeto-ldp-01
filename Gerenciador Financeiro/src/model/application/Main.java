4528e7456c75a41c8fad657a39e88a46720ff0b2
Adriel Felipe <adrielfelipevieira@gmail.com>
2026-05-28 01:12:23 -0300

    adicionando a opção remover por nome

--- Gerenciador Financeiro/src/model/application/Main.java
+++ Gerenciador Financeiro/src/model/application/Main.java
@@ -17,8 +17,8 @@ public class Main {
 
     public static void main() {
 
-        System.out.println("Bem vindo ao seu Gerenciador Financeiro!");
-        double saldoInicial = lerNumero("Digite o seu saldo inicial: ");
+        imprimiTexto("Bem vindo ao seu Gerenciador Financeiro!");
+        double saldoInicial = lerDouble("Digite o seu saldo inicial: ");
 
         int opc;
 
@@ -38,11 +38,14 @@ public class Main {
                 case 3:
                     calcularSaldoTotal(saldoInicial);
                     break;
+                case 4:
+                    removerMovimentacaoPorNome();
+                    break;
                 default:
-                    System.out.println("Erro: Digite uma opção válida!");
+                    imprimiTexto("Erro: Digite uma opção válida!");
             }
         } while (opc != 0);
-        System.out.println("Programa encerrado.");
+        imprimiTexto("Programa encerrado.");
         sc.close();
     }
 
@@ -50,17 +53,17 @@ public class Main {
 
     public static void cadastroMovimentacao() {
         if (qtdAtual >= LIMITE_TRANSACOES) {
-            System.out.println("Limite de transações atingido!");
+            imprimiTexto("Limite de transações atingido!");
             return;
         }
 
-        double value = lerNumero("Valor: ");
+        double value = lerDouble("Valor: ");
         sc.nextLine();
         String desc = lerTexto("Descrição: ");
 
         historico[qtdAtual] = new Movimentacao(value, desc);
         qtdAtual++;
-        System.out.println("Gasto adicionado com sucesso!");
+        imprimiTexto("Gasto adicionado com sucesso!");
     }
 
 
@@ -68,18 +71,19 @@ public class Main {
         System.out.println("\n--- GERENCIADOR FINANCEIRO ---");
         System.out.println("1. Adicionar Gasto");
         System.out.println("2. Ver Extrato");
-        System.out.println("3. Calcular Gasto Total e Slado Atual");
+        System.out.println("3. Calcular Gasto Total e Saldo Atual");
+        System.out.println("4. Remover Gasto por Nome.");
         System.out.println("0. Sair");
         System.out.println("--------------------------------");
 
     }
 
     public static void listarMovimentacao () {
-        System.out.println("=====================================");
+        imprimiTexto("\n=====================================");
         for (int i = 0; i < qtdAtual; i++) {
             historico[i].exibirResumo();
         }
-        System.out.println("=====================================");
+        imprimiTexto("=====================================");
     }
 
     public static void calcularSaldoTotal(double saldoInicial) {
@@ -87,13 +91,43 @@ public class Main {
         for (int i = 0; i < qtdAtual; i++) {
             total += historico[i].value;
         }
-        System.out.println("==========================");
+        System.out.println("\n==========================");
         System.out.printf("Total de Gastos: R$ %.2f%n", total);
         System.out.printf("Saldo Restante: R$ %.2f%n", saldoInicial - total);
         System.out.println("==========================");
     }
 
-    public static double lerNumero(String msg) {
+    public static void removerMovimentacaoPorNome() {
+        if (qtdAtual == 0) {
+            System.out.println("Nenhum gasto cadastrado para remover.");
+        }
+
+        System.out.println("Listagem do Extrato para Ver o Nome...");
+        listarMovimentacao();
+        String nomeProcurado = lerTexto("Digite o nome (descrição) do gasto que deseja remover: ");
+
+
+        boolean indexProcurado = false;
+        for (int i = 0; i < qtdAtual; i++) {
+            if(historico[i].desc.equalsIgnoreCase(nomeProcurado)) {
+                indexProcurado = true;
+                for (int j = i; j < qtdAtual -1 ; j++) {
+                    historico[j] = historico[j+1];
+                }
+            }
+        }
+        if (indexProcurado) {
+            historico[qtdAtual-1] = null;
+            qtdAtual--;
+            System.out.println("Gasto '" + nomeProcurado + "' removido com sucesso!");
+        } else {
+            System.out.println("Gasto '" + nomeProcurado + "' não foi encontrado.");
+        }
+
+
+    }
+
+    public static double lerDouble(String msg) {
         System.out.println(msg);
         return sc.nextDouble();
     }
@@ -105,6 +139,10 @@ public class Main {
     public static String lerTexto(String msg) {
         System.out.println(msg);
         return sc.nextLine();
+    }
+
+    public static void imprimiTexto(String msg) {
+        System.out.println(msg);
     }
 
 }
