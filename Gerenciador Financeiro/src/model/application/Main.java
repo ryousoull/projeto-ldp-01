7aeb282765c0daba33c2a46ce3b14572448763b0
Adriel Felipe <adrielfelipevieira@gmail.com>
2026-05-28 00:44:03 -0300

    reorganização do projeto

--- Gerenciador Financeiro/src/model/application/Main.java
+++ Gerenciador Financeiro/src/model/application/Main.java
@@ -1,55 +1,112 @@
 package model.application;
-
 import model.entities.Movimentacao;
-import model.utilities.Uteis;
+
 
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
+    static Scanner sc = new Scanner(System.in);
     static final int LIMITE_TRANSACOES = 100;
 
-    static void main() {
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
+    static Movimentacao [] historico = new Movimentacao[LIMITE_TRANSACOES];
+    static int qtdAtual = 0;
 
-        Uteis util = new Uteis();
-        Movimentacao[] historico = new Movimentacao[LIMITE_TRANSACOES];
 
-        System.out.print("Digite o seu saldo inicial: ");
-        double saldoInicial = sc.nextDouble();
 
-        int opc, contador = 0;
+    public static void main() {
+
+        System.out.println("Bem vindo ao seu Gerenciador Financeiro!");
+        double saldoInicial = lerNumero("Digite o seu saldo inicial: ");
+
+        int opc;
 
         do {
-            util.exibirMenu();
-            opc = sc.nextInt();
+            exibirMenu();
+            opc = lerInt("Escolha: ");
             sc.nextLine();
+
+
             switch (opc) {
                 case 1:
-                    if (contador < LIMITE_TRANSACOES) {
-                        System.out.print("Descrição: ");
-                        String desc = sc.nextLine();
-                        System.out.print("Valor: ");
-                        double valor = sc.nextDouble();
-                        historico[contador] = new Movimentacao(valor, desc);
-                        contador++;
-                    } else {
-                        System.out.println("Limite de transações atingido!");
-                    }
+                    cadastroMovimentacao();
                     break;
                 case 2:
-                    util.listarMovimentacao(historico, contador);
+                    listarMovimentacao();
                     break;
                 case 3:
-                    util.mostrarSaldoAtual(historico, contador, saldoInicial);
+                    calcularSaldoTotal(saldoInicial);
                     break;
+                default:
+                    System.out.println("Erro: Digite uma opção válida!");
             }
         } while (opc != 0);
         System.out.println("Programa encerrado.");
         sc.close();
     }
+
+
+
+    public static void cadastroMovimentacao() {
+        if (qtdAtual >= LIMITE_TRANSACOES) {
+            System.out.println("Limite de transações atingido!");
+            return;
+        }
+
+        double value = lerNumero("Valor: ");
+        sc.nextLine();
+        String desc = lerTexto("Descrição: ");
+
+        historico[qtdAtual] = new Movimentacao(value, desc);
+        qtdAtual++;
+        System.out.println("Gasto adicionado com sucesso!");
+    }
+
+
+    public static void exibirMenu() {
+        System.out.println("\n--- GERENCIADOR FINANCEIRO ---");
+        System.out.println("1. Adicionar Gasto");
+        System.out.println("2. Ver Extrato");
+        System.out.println("3. Calcular Gasto Total e Slado Atual");
+        System.out.println("0. Sair");
+        System.out.println("--------------------------------");
+
+    }
+
+    public static void listarMovimentacao () {
+        System.out.println("=====================================");
+        for (int i = 0; i < qtdAtual; i++) {
+            historico[i].exibirResumo();
+        }
+        System.out.println("=====================================");
+    }
+
+    public static void calcularSaldoTotal(double saldoInicial) {
+        double total = 0;
+        for (int i = 0; i < qtdAtual; i++) {
+            total += historico[i].value;
+        }
+        System.out.println("==========================");
+        System.out.printf("Total de Gastos: R$ %.2f%n", total);
+        System.out.printf("Saldo Restante: R$ %.2f%n", saldoInicial - total);
+        System.out.println("==========================");
+    }
+
+    public static double lerNumero(String msg) {
+        System.out.println(msg);
+        return sc.nextDouble();
+    }
+    public static int lerInt(String msg) {
+        System.out.println(msg);
+        return sc.nextInt();
+    }
+
+    public static String lerTexto(String msg) {
+        System.out.println(msg);
+        return sc.nextLine();
+    }
+
 }
 
 
--- Gerenciador Financeiro/src/model/entities/Movimentacao.java
+++ Gerenciador Financeiro/src/model/entities/Movimentacao.java
@@ -1,27 +1,11 @@
 package model.entities;
 
 public class Movimentacao {
-    private Double value;
-    private String desc;
+    public Double value;
+    public String desc;
 
     public Movimentacao(Double value, String desc) {
         this.value = value;
-        this.desc = desc;
-    }
-
-    public Double getValue() {
-        return value;
-    }
-
-    public void setValue(Double value) {
-        this.value = value;
-    }
-
-    public String getDesc() {
-        return desc;
-    }
-
-    public void setDesc(String desc) {
         this.desc = desc;
     }
 
